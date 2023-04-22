47df1a9c3f6c5a0cd96be398599ea529a8f52818
GabrielLoeza03443 <j.loeza@softrek.mx>
2023-04-22 03:37:10 -0600

    base de datos

--- database/v1.sql
+++ database/v1.sql
@@ -58,13 +58,14 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `_token` varchar(200) DEFAULT '',
   PRIMARY KEY (`id`),
   KEY `FK__category` (`id_category`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla echamelamano.clients: 1 rows
+-- Volcando datos para la tabla echamelamano.clients: 2 rows
 /*!40000 ALTER TABLE `clients` DISABLE KEYS */;
 INSERT INTO `clients` (`uuid`, `id`, `email`, `phone`, `andress`, `description`, `validate`, `id_category`, `photo`, `rang`, `active`, `name`, `last_name`, `suscription`, `password`, `_token`) VALUES
 	('864e85df-003d-4019-98f7-5bf630c896c7', 1, 'gabriel@echamelamani.com', '911026504', '11sqs2w21  21 e2', '', '1', '1', '1681547518_643a60fe544f3.jpg', '0', 1, 'Jesus Gabriel', 'Loeza Conrrado', 1, '$2y$10$k3pCNzpnbemOTq5vB7yPYexZO37SijrBazQcCuhJqXNTqvCUN2zva', ''),
-	('5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 2, 'test@test.com', '991289127817', 'en kanasin', '', '1', '1', '1681547659_643a618bc91dc.jpg', '0', 1, 'Gilberto', 'Aviles', 1, '$2y$10$5fR.6ModypXFQwphsiT7/.KM4m8AbqEu5sf1BMD8AG8Pi8xo45EhG', '');
+	('5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 2, 'test@test.com', '991289127817', 'en kanasin', '', '1', '1', '1681547659_643a618bc91dc.jpg', '0', 1, 'Gilberto', 'Aviles', 1, '$2y$10$5fR.6ModypXFQwphsiT7/.KM4m8AbqEu5sf1BMD8AG8Pi8xo45EhG', ''),
+	('eff19ce6-faed-4e99-a0f1-8e70f4a4ddcd', 3, 'gabriel26loeza04@gmail.com', '9911038094', 'calle 23x24 y 26', '', '1', '1', '1682154919_6443a5a73d2ce.jpg', '0', 1, 'Gabo', 'Loeza', 1, '$2y$10$xafL9VPQOPp/F3qCwamCYO5pHRNnApykqrPN8VojZ5cTjwaGsI7ii', '');
 /*!40000 ALTER TABLE `clients` ENABLE KEYS */;
 
 -- Volcando estructura para tabla echamelamano.comments
@@ -72,15 +73,32 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comments_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `publications_id` bigint(20) DEFAULT '0',
   `id` bigint(20) DEFAULT '0',
-  `content` text,
+  `comentario` text,
   `id_user_comments` text,
   `reactions` bigint(20) DEFAULT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`comments_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla echamelamano.comments: 0 rows
+-- Volcando datos para la tabla echamelamano.comments: 2 rows
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` (`comments_id`, `publications_id`, `id`, `comentario`, `id_user_comments`, `reactions`, `date`) VALUES
+	(1, 6, 1, 'Mi primer comentario', '864e85df-003d-4019-98f7-5bf630c896c7', 0, '2023-04-22 02:02:27'),
+	(2, 6, 2, 'Sigue disponible?', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 4, '2023-04-22 02:28:24'),
+	(3, 6, 0, 'Hola', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:07:09'),
+	(4, 6, 0, 'sqss', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:08:21'),
+	(5, 8, 0, 'hola', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:10:16'),
+	(6, 8, 0, 't', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:11:30'),
+	(7, 8, 0, 'fwfw', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:12:49'),
+	(8, 9, 0, 'Yo puedo apoyarte', 'eff19ce6-faed-4e99-a0f1-8e70f4a4ddcd', 0, '2023-04-22 09:15:48'),
+	(9, 9, 0, 'encerio?', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:15:58'),
+	(10, 9, 0, 'Si', 'eff19ce6-faed-4e99-a0f1-8e70f4a4ddcd', 0, '2023-04-22 09:19:21'),
+	(11, 9, 0, 'Me interesa amigo.', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:19:32'),
+	(12, 9, 0, 'Solicita el servicio amigo.', 'eff19ce6-faed-4e99-a0f1-8e70f4a4ddcd', 0, '2023-04-22 09:23:32'),
+	(13, 9, 0, 'Okey te solito la mano.', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:23:51'),
+	(14, 1, 0, 'Hola', 'eff19ce6-faed-4e99-a0f1-8e70f4a4ddcd', 0, '2023-04-22 09:29:33'),
+	(15, 1, 0, 'Como estas?', 'eff19ce6-faed-4e99-a0f1-8e70f4a4ddcd', 0, '2023-04-22 09:29:54'),
+	(16, 1, 0, 'Bien y tu como estas?', '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', 0, '2023-04-22 09:30:02');
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 
 -- Volcando estructura para tabla echamelamano.failed_jobs
@@ -159,9 +177,9 @@ CREATE TABLE IF NOT EXISTS `publications` (
   `date` datetime DEFAULT NULL,
   `uuid` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`publications_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla echamelamano.publications: 2 rows
+-- Volcando datos para la tabla echamelamano.publications: 6 rows
 /*!40000 ALTER TABLE `publications` DISABLE KEYS */;
 INSERT INTO `publications` (`publications_id`, `status`, `content`, `reactions`, `id_user`, `date`, `uuid`) VALUES
 	(1, 1, 'Hola', 0, '864e85df-003d-4019-98f7-5bf630c896c7', '2023-04-15 08:34:28', '7ac06881-1ba6-4dba-a34e-e60914b79541'),
@@ -169,7 +187,10 @@ INSERT INTO `publications` (`publications_id`, `status`, `content`, `reactions`,
 	(3, 1, '1000', 0, '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', '2023-04-15 08:42:38', '17fba492-9e93-48dd-931c-04e2a7728f82'),
 	(4, 1, 'Mi primera publicacion.', 0, '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', '2023-04-15 08:44:30', 'a9293289-9f67-4439-b541-a34dd2d66a7e'),
 	(5, 1, 'Hola mundo', 0, '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', '2023-04-15 08:45:02', '7b1254e5-dbd6-4a07-80d3-b526ad01cbe6'),
-	(6, 1, 'Algún carpintero cerca de kanasin', 0, '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', '2023-04-15 08:47:03', '64fbe6b5-78ee-44dd-85fa-8f0a2ea7313e');
+	(6, 1, 'Algún carpintero cerca de kanasin', 0, '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', '2023-04-15 08:47:03', '64fbe6b5-78ee-44dd-85fa-8f0a2ea7313e'),
+	(7, 1, 'Hola', 0, '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', '2023-04-22 08:54:30', '6fe83be0-f06c-4aba-8f72-4e41a09511fb'),
+	(8, 1, 'Otro', 0, '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', '2023-04-22 09:05:06', '5f186bde-c990-4db7-8bd6-afb570cdc2bb'),
+	(9, 1, 'Necesito dj para mañana a las 12 pm del dia', 0, '5bf8b9a7-95ce-40b4-86eb-1bc2314c0b14', '2023-04-22 09:15:38', '376ec622-ea50-4635-86a8-baeb950e9dc1');
 /*!40000 ALTER TABLE `publications` ENABLE KEYS */;
 
 -- Volcando estructura para tabla echamelamano.sub_category
