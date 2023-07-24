0ec54c6486e6b4544180d5e895a4741c80d3b35e
GabrielLoeza03443 <j.loeza@softrek.mx>
2023-07-23 18:50:50 -0600

    reparacion de create view mobile

--- resources/views/site/create.blade.php
+++ resources/views/site/create.blade.php
@@ -2,19 +2,19 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
     <title>Crear Usuario</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/layouts.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
 </head>
 
 <body>
-    <main class="vh-100">
-        <section class="container-fluid">
+    <section class="vh-100">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-md-6 text-black">
+                <div class="col-sm-6 text-black">
 
                     <div class="px-5 ms-xl-4">
                         <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
@@ -22,7 +22,8 @@
                     </div>
 
                     <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-                        <form style="width: 50rem;" method="post"action="{{ route('crear_cliente') }}">
+
+                   <form style="width: 50rem;" method="post"action="{{ route('crear_cliente') }}">
                             @csrf
                             <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Crear Usuario</h3>
 
@@ -54,26 +55,31 @@
                             </div>
 
                         </form>
+
                     </div>
                     {{-- <div class="mt-5 mb-4">
-                        <a href="auth/google/register">
+                        <a href="auth/google">
                             <div class="bg-white text-center p-2">
                                 <button class="btn btn-white text-danger">
-                                    Crear cuenta con
+                                    Iniciar sesión con
                                     <i class="fab fa-google me-2"></i>oogle
                                 </button>
                             </div>
                         </a>
                     </div> --}}
+
+                    {{-- <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">¿Has olvidado tu
+                        contraseña?
+                    </a></p> --}}
+                {{-- <p>¿No tienes una cuenta? <a href="crear_client" class="link-info">Registrar aquí</a></p> --}}
                 </div>
-                <div class="col-md-6 px-0 d-none d-sm-block">
+                <div class="col-sm-6 px-0 d-none d-sm-block">
                     <img src="img/icon.jpg" alt="Login image" class="w-100 vh-100"
                         style="object-fit: cover; object-position: left;">
                 </div>
             </div>
-        </section>
-    </main>
-
+        </div>
+    </section>
 </body>
 
 </html>
