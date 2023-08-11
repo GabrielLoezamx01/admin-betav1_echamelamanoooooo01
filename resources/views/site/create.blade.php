2a015c6652c4e3905d57ac70cf332636bc455571
GabrielLoeza03443 <j.loeza@softrek.mx>
2023-08-11 03:05:55 -0600

    cambios de diseño de create

--- resources/views/site/create.blade.php
+++ resources/views/site/create.blade.php
@@ -1,85 +1,173 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Crear Usuario</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EchameLaMano - Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="css/layouts.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
+        body,
+        input,
+        label,
+        p,
+        h1,
+        h2,
+        b,
+        textarea,
+        select,
+        checkbox {
+            font-family: 'Poppins', sans-serif;
+        }
+
+        body {
+            background-color: #FAFFFD;
+        }
+
+        .text-site {
+            color: #249F11;
+        }
+
+        .btn-one {
+            background-color: #342E37;
+            color: #FAFFFD;
+        }
+
+        .efect {
+            position: relative;
+            margin-bottom: 1rem;
+        }
+
+        .efect input {
+            width: 100%;
+            border: none;
+            border-bottom: 1px solid #342E37;
+            padding: 0.5rem;
+            font-size: 1rem;
+            outline: none;
+        }
+
+        .efect label {
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform-origin: top left;
+            transform: translateY(1rem);
+            font-size: 0.875rem;
+            /* Ajusta el tamaño de la fuente según tus preferencias */
+            color: #666;
+            transition: transform 0.2s ease-out, font-size 0.2s ease-out, color 0.2s ease-out;
+        }
+
+        .efect input:focus+label,
+        .efect input:not(:placeholder-shown)+label {
+            transform: translateY(0);
+            font-size: 0.75rem;
+            /* Ajusta el tamaño de la fuente según tus preferencias */
+            color: #333;
+        }
+
+        .efect input:not(:placeholder-shown)+label {
+            transform: translateY(0);
+            font-size: 1rem;
+            /* Ajusta el tamaño de la fuente según tus preferencias */
+            color: #333;
+            padding-left: 0.5rem;
+        }
+
+        @keyframes bounce {
+
+            0%,
+            20%,
+            50%,
+            80%,
+            100% {
+                transform: translateY(0);
+            }
+
+            40% {
+                transform: translateY(-20px);
+            }
+
+            60% {
+                transform: translateY(-10px);
+            }
+        }
+
+        .animated-h1 {
+            animation: bounce 2s infinite;
+        }
+
+        .background-image {
+            background-image: url('img/icon.jpg');
+            background-size: cover;
+            background-position: center;
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            left: 0;
+            opacity: 0.04;
+            /* Cambia el valor para ajustar la opacidad */
+            z-index: -1;
+        }
+
+        .custom-select:focus {
+            border-color: #249F11;
+            box-shadow: 0 0 0 0.25rem rgba(36, 159, 17, 0.25);
+        }
+    </style>
 </head>
 
 <body>
-    <section class="vh-100">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-6 text-black">
-
-                    <div class="px-5 ms-xl-4">
-                        <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-                        <span class="h1 fw-bold mb-0">Logo</span>
-                    </div>
-
-                    <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-
-                   <form style="width: 50rem;" method="post"action="{{ route('crear_cliente') }}">
-                            @csrf
-                            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Crear Usuario</h3>
-
-                            <div class="mb-3">
-                                <label for="">Correo:</label>
-                                <input class="form-control custom-focus mt-3 form-control-lg" type="text"
-                                    name="email" autocomplete="off" value="{{ old('email') }}">
-                            </div>
-                            <div class="mb-3">
-                                <label for="">Contraseña:</label>
-                                <input class="form-control custom-focus mt-3 form-control-lg" type="password"
-                                    name="password" id="p1" autocomplete="off" value="{{ old('password') }}">
-                            </div>
-                            {{-- <div class="mb-3">
-                                <label for="">Confirma Contraseña:</label>
-                                <input class="form-control custom-focus mt-3 form-control-lg" type="password"
-                                    name="password2" id="p2" autocomplete="off" value="{{ old('password2') }}">
-                            </div> --}}
-                            <div class="mb-3">
-                                <label for="">Tipo de usuario:</label>
-                                <select name="type_client" id="type_client" class="form-control custom-focus">
-                                    <option value="type_one">Vendedor</option>
-                                    <option value="type_two">Cliente</option>
-                                </select>
-                            </div>
-
-                            <div class="text-center">
-                                <button class="btn btn-large btn-dark p-2">Crear</button>
-                            </div>
-
-                        </form>
-
-                    </div>
-                    {{-- <div class="mt-5 mb-4">
-                        <a href="auth/google">
-                            <div class="bg-white text-center p-2">
-                                <button class="btn btn-white text-danger">
-                                    Iniciar sesión con
-                                    <i class="fab fa-google me-2"></i>oogle
-                                </button>
-                            </div>
-                        </a>
-                    </div> --}}
-
-                    {{-- <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">¿Has olvidado tu
-                        contraseña?
-                    </a></p> --}}
-                {{-- <p>¿No tienes una cuenta? <a href="crear_client" class="link-info">Registrar aquí</a></p> --}}
-                </div>
-                <div class="col-sm-6 px-0 d-none d-sm-block">
-                    <img src="img/icon.jpg" alt="Login image" class="w-100 vh-100"
-                        style="object-fit: cover; object-position: left;">
-                </div>
+    <div class="background-image"></div>
+    <main class="container mt-5">
+        <div class="row d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+            <div class="col-md-6 col-xl-6 col-lg-6 p-5 order-md-1">
+                <img src="svg/one.svg" class="img-fluid">
             </div>
+            <div class="col-md-6 col-xs-6 col-lg-6  p-5 order-md-2">
+                <h2 class="text-site fw-bold fs-1 animated-h1">EchameLaMano</h2>
+                <div class="mt-5">
+                </div>
+                <form method="post" action="{{ route('crear_cliente') }}">
+                    @csrf
+                    <input type="hidden" name="rol" value="001">
+                    {{-- <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3> --}}
+                    <div class="efect mt-5">
+                        <input type="email" name="email" id="email" autocomplete="off" placeholder=" "
+                            value="{{ old('email') }}" class="mt-3" />
+                        <label for="email">Correo</label>
+                    </div>
+                    <div class="efect mt-5">
+                        <input type="password" name="password" autocomplete="off" placeholder=" " class="mt-3" />
+                        <label class="form-label" for="form2Example28" value="{{ old('password') }}">Contraseña</label>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="">Tipo de usuario:</label>
+                        <select name="type_client" id="type_client" class="custom-focus form-control">
+                            <option value="type_one">Vendedor</option>
+                            <option value="type_two">Cliente</option>
+                        </select>
+                    </div>
+                    <div class="text-center">
+                        <button class="btn btn-large btn-dark p-2">Crear</button>
+                    </div>
+
+                </form>
+            </div>
+
         </div>
-    </section>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="css/layouts.css">
     <style>
         @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
         body,
         input,
         label,
@@ -116,9 +117,11 @@
             z-index: -1;
         }
 
-        a.text-site {
-            color: #249F11;
+        a {
+            color: #249F11 !important;
+            text-decoration: none !important;
         }
+
     </style>
 </head>
 
