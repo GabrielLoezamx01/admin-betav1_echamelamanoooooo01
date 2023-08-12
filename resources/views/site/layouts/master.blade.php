036d1d6781a6da97b72343cbb7abe52d56198640
GabrielLoeza03443 <j.loeza@softrek.mx>
2023-08-11 20:14:07 -0600

    diseño nuevo de poss fotos v5

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -427,7 +427,8 @@
         }
 
         .profile-image-container img {
-            max-width: 400pxs;
+            max-width: 400px;
+            height: 120px;
             border: 10px solid #4caf50;
             /* Agregar un borde blanco */
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
